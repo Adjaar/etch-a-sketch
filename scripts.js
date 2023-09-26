a2b05e35b56f420dd10589b139c7696345281d33
Adriel Davila <adriel.davila@outlook.com>
2023-09-26 05:32:12 -0400

    Add javascript logic with flexbox for dynamic grid

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-const container = document.getElementsByClassName('gridContainer');
+const container = document.getElementById('gridContainer');
 
 function makeGrid(numDivs) {
     for (let i = 0; i < numDivs; i++){
         let cells = document.createElement('div');
-        cells.classList.add('grid');
+         cells.classList.add('grid');
+         cells.style.width = (100 / Math.sqrt(numDivs)) + "%";
+         cells.style.height = (100 / Math.sqrt(numDivs)) + "%";
+         console.log("hi my name is, " + cells.style.width);
         container.appendChild(cells);
     }
 }
